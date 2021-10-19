e12ddc347d229419dd198c1fc10a81e2de52b3c0
miqunore <82279998+miqunore@users.noreply.github.com>
2021-10-19 13:07:06 +0300

    add chekbox

--- js/script.js
+++ js/script.js
@@ -4,11 +4,28 @@ const work1 = {};
 
 const listWork = document.querySelector('#list_work');
 
+function fun1(i) {
+     let chBox;
+     let valueInBox = document.querySelector(`#labelCheckBox${i}`);
+     chBox = document.querySelector(`#checkBoxs${i}`);
+     if (chBox.checked) {
+        work1[valueInBox.textContent] = false;
+        valueInBox.style.textDecoration = 'line-through';
+     } else {
+        work1[valueInBox.textContent] = true;
+        valueInBox.style.textDecoration = 'none';
+     }
+}
+
+let i = 0;
+
 ADD_BUTTON.onclick = () => {
-    work1[document.querySelector('input').value] = 1;
-    console.log(work1);
-    console.log(work1.value);
-    listWork.innerHTML += `<input type="checkbox"><label>${document.querySelector('input').value}</label>`
+    work1[document.querySelector('input').value] = true;
+    listWork.innerHTML += `            <div class="all_works">
+    <input type="checkbox" id="checkBoxs${i}" onchange="fun1(${i})">
+    <label id="labelCheckBox${i}">${document.querySelector('input').value}</label>
+</div>`
     document.querySelector('input').value = '';
+    i++
 }
 
