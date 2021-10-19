1a09c1381f7d3d0434b6f9a5ef672dd82c2dc9a6
miqunore <82279998+miqunore@users.noreply.github.com>
2021-10-19 13:50:44 +0300

    add button delete

--- js/script.js
+++ js/script.js
@@ -4,10 +4,10 @@ const work1 = {};
 
 const listWork = document.querySelector('#list_work');
 
-function fun1(i) {
+function onOffCheckBox(y) {
      let chBox;
-     let valueInBox = document.querySelector(`#labelCheckBox${i}`);
-     chBox = document.querySelector(`#checkBoxs${i}`);
+     let valueInBox = document.querySelector(`#labelCheckBox${y}`);
+     chBox = document.querySelector(`#checkBoxs${y}`);
      if (chBox.checked) {
         work1[valueInBox.textContent] = false;
         valueInBox.style.textDecoration = 'line-through';
@@ -19,12 +19,19 @@ function fun1(i) {
 
 let i = 0;
 
+function buttonDelite(x) {
+    let obj = document.querySelector(`#labelCheckBox${x}`).textContent;
+    document.querySelector(`#divBlock${x}`).remove();
+    delete work1[obj];
+}
+
 ADD_BUTTON.onclick = () => {
     work1[document.querySelector('input').value] = true;
-    listWork.innerHTML += `            <div class="all_works">
-    <input type="checkbox" id="checkBoxs${i}" onchange="fun1(${i})">
+    listWork.innerHTML += `            <div class="all_works" id="divBlock${i}">
+    <input type="checkbox" id="checkBoxs${i}" onchange="onOffCheckBox(${i})">
     <label id="labelCheckBox${i}">${document.querySelector('input').value}</label>
-</div>`
+    <button onclick="buttonDelite(${i})" id="buttonDelide${i}" class="btn_del">X</button>
+    </div>`
     document.querySelector('input').value = '';
     i++
 }
