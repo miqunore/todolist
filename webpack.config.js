f2ad8807deb2adb15e7775c3690eea3d0af9d864
miqunore <82279998+miqunore@users.noreply.github.com>
2021-10-29 13:07:12 +0300

    add HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+
 
 module.exports = {
   entry: './src/core/index.js',
@@ -6,4 +8,11 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
+  plugins: [
+    new HtmlWebpackPlugin(), // Generates default index.html
+    new HtmlWebpackPlugin({  // Also generate a test.html
+      filename: 'index.html',
+      template: 'src/index.html'
+    })
+  ]
 };
