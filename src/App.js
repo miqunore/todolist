ba283c72e5249b7f740d526f667d77ca0562199b
miqunore <82279998+miqunore@users.noreply.github.com>
2021-11-10 12:45:21 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,1 +1,25 @@
 import {  } from './layout/Layout.js';
+
+
+export function App() {
+    return `
+    <section class="section_top">
+        <h1>Список дел:</h1>
+        <div class="add_work">
+            <input type="text" placeholder="добавить дело" id="addWork">
+            <button id=buttonWork>Добавить</button>
+        </div>
+
+        <div>
+            <input type="radio" id="radioAll" name="radiobutton" value="radiobutton" checked>Всё
+            <input type="radio" id="radioActiv" name="radiobutton" value="radiobutton">Активное
+            <input type="radio" id="radioNoActiv" name="radiobutton" value="radiobutton">Не активное
+        </div>
+
+        <div class="work1" id="list_work">
+            
+           
+        </div>
+    </section>
+    `;
+}
--- src/index.js
+++ src/index.js
@@ -1,29 +1,8 @@
 import { render } from './core/index.js';
 import './index.scss';
-import {  } from './App.js';
+import { App } from './core/App.js';
 
 
-function TodoApp() {
-    return `
-    <section class="section_top">
-        <h1>Список дел:</h1>
-        <div class="add_work">
-            <input type="text" placeholder="добавить дело" id="addWork">
-            <button id=buttonWork>Добавить</button>
-        </div>
 
-        <div>
-            <input type="radio" id="radioAll" name="radiobutton" value="radiobutton" checked>Всё
-            <input type="radio" id="radioActiv" name="radiobutton" value="radiobutton">Активное
-            <input type="radio" id="radioNoActiv" name="radiobutton" value="radiobutton">Не активное
-        </div>
-
-        <div class="work1" id="list_work">
-            
-           
-        </div>
-    </section>
-    `;
-}
 
 render(TodoApp, {}, document.getElementById('root'))
