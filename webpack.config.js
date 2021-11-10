caf564880a30e903ca546573da4c77e86af42582
Artem Malieiev <artem.malieiev@namecheap.com>
2021-11-10 16:28:32 +0200

    added devtool config

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,33 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
-
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: 'src/index.html'
-    })
-  ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'public'),
+    devtool: "source-map",
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
     },
-    open: true,
-    compress: true,
-    port: 9000,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.s?ss$/i,
-        use: [
-          "style-loader",
-          "css-loader",
-          "sass-loader",
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            template: "src/index.html",
+        }),
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "public"),
+        },
+        open: true,
+        compress: true,
+        port: 9000,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s?ss$/i,
+                use: ["style-loader", "css-loader", "sass-loader"],
+            },
         ],
-      },
-    ]
-    
-  }
+    },
 };
